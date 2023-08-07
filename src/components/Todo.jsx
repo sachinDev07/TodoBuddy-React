ffadfc9c1141d73a39465d14a93d9dae4822e470
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-08-07 19:18:52 +0500

    create TodoList component and implemented localStorage in Todo component

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { BsChevronDown } from "react-icons/bs";
 
 import TaskCard from "./TaskCard";
+import TodoList from "./TodoList";
 
 const Todo = () => {
   const [cardShow, setCardShow] = useState(false);
@@ -10,38 +11,58 @@ const Todo = () => {
     setCardShow(true);
   };
 
+  const handleSubmitTask = (taskDetails) => {
+    const updatedTaskData = [...storedTaskData, taskDetails];
+    localStorage.setItem("tasks", JSON.stringify(updatedTaskData));
+    setCardShow(false);
+  };
+
+  const storedTaskData = JSON.parse(localStorage.getItem("tasks")) || [];
+
   return (
     <section className="px-4 mt-8 relative">
-      <div className="max-w-[1000px] mx-auto flex justify-between">
-        <button
-          type="button"
-          onClick={addTask}
-          className="px-4 py-2 text-lg text-[#00214d] bg-[#00ebc7] font-semibold rounded hover:bg-cyan-400 cursor-pointer transition duration-150 ease-in-out "
-        >
-          Add New Task
-        </button>
-        <div className="flex items-center space-x-6 text-gray-700 font-semibold ">
-          <button className="flex items-center">
-            Sort <BsChevronDown className="ml-1 text-black" />
+      <div className="relative max-w-[1000px] mx-auto flex flex-col justify-between">
+        <div className="flex justify-between">
+          <button
+            type="button"
+            onClick={addTask}
+            className="px-4 py-2 text-lg text-[#00214d] bg-[#00ebc7] font-semibold rounded hover:bg-cyan-400 cursor-pointer transition duration-150 ease-in-out "
+          >
+            Add New Task
           </button>
-          <button className="flex items-center">
-            Filter <BsChevronDown className="ml-1 text-black" />
-          </button>
-          <button className="flex items-center">
-            Category <BsChevronDown className="ml-1 text-black" />
+          <div className="flex items-center space-x-6 text-gray-700 font-semibold ">
+            <button className="flex items-center">
+              Sort <BsChevronDown className="ml-1 text-black" />
+            </button>
+            <button className="flex items-center">
+              Filter <BsChevronDown className="ml-1 text-black" />
+            </button>
+            <button className="flex items-center">
+              Category <BsChevronDown className="ml-1 text-black" />
+            </button>
+          </div>
+        </div>
+
+        <div className="flex justify-end mt-12 my-2">
+          <button className=" text-gray-700 font-semibold px-2 py-2 bg-gray-300 rounded-md hover:bg-gray-200 active:bg-gray-300 border border-gray-300 transition duration-150 ease-in-out">
+            Clear All
           </button>
         </div>
-      </div>
 
-      <div className="flex justify-center">
-      {cardShow && (
-        <div className="">
-          <TaskCard cardShow={cardShow}/>
+        <div className="absolute top-[30%] md:top-[40%] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          {cardShow && (
+            <div className="">
+              <TaskCard onSubmitTask={handleSubmitTask} />
+            </div>
+          )}
         </div>
-      )}
-
+        <div className="sm:grid sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-4 mt-2 mb-6 gap-4 overflow-hidden">
+          {storedTaskData.length > 0 &&
+            storedTaskData?.map((task) => (
+              <TodoList key={task.name} task={task} />
+            ))}
+        </div>
       </div>
-        
     </section>
   );
 };
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import { AiFillEdit } from "react-icons/ai";
+import { TiTick } from "react-icons/ti";
+
+const TodoList = ({ task }) => {
+  const [truncateText, setTruncateText] = useState("");
+  const { name, startTime, endTime, date } = task;
+
+  useEffect(() => {
+    if (name.length > 30) {
+      setTruncateText(name.slice(0, 30) + "...");
+    } else {
+      setTruncateText(name);
+    }
+  }, [name]);
+
+  return (
+    <div className="mb-6 max-h-68 md:mb-2 flex flex-col justify-between bg-red-200 p-3 rounded-md shadow-md hover:shadow-lg  overflow-hidden">
+      <div className="flex justify-between items-center mb-4">
+        <p className="text-xs font-semibold text-gray-700 ">{date}</p>
+        <TiTick
+          title="Click to complete the task"
+          className="text-xl bg-white rounded-full text-green-600 cursor-pointer border border-green-300 hover:border-green-600"
+        />
+      </div>
+      <h1 className={`text-5xl md:text-3xl text-gray-800 font-semibold `}>
+        {truncateText}
+      </h1>
+      <div className="flex items-center justify-between mt-8">
+        <div className="flex items-center space-x-1 text-base text-gray-700 font-semibold">
+          <p className="">{startTime}</p>
+          <span className="font-bold text-lg text-gray-700">-</span>
+          <p className="">{endTime}</p>
+        </div>
+        <div className="flex space-x-3 items-center">
+          <AiFillEdit
+            title="Edit"
+            className="cursor-pointer font-bold text-lg text-green-700 hover:text-green-600 transition duration-150 ease-in-out"
+          />
+          <p
+            title="Remove"
+            className="cursor-pointer font-bold text-lg text-red-700 hover:text-red-500 transition duration-150 ease-in-out"
+          >
+            X
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
